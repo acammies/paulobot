dcf9c827ec583eb67aa5a98a73132e3319e68c3b
acammies <acammies@redhat.com>
2018-05-07 20:39:22 +0100

    added echo feature to test Openshift cicd

--- index.js
+++ index.js
@@ -15,20 +15,34 @@ contribute at 'https://github.com/acammies/paulobot'
         `)
     );
 });
+
 bot.on(['/hello', '/start', '/paulo'], (msg) => {
     console.log(`/hello, /start, /paulo triggered`)
     return bot.sendMessage(msg.chat.id, `Welcome, ${ msg.from.first_name } \u{1F602} !`);
 });
 
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+  
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+  
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+  });
+
 bot.on('/welcomeall', (msg) => {
     console.log(`/welcomeall triggered`)
     return [
-        bot.sendMessage(msg.chat.id,  `Welcome ${ msg.from.first_name } \u{1F602}`),
-        bot.sendMessage(msg.chat.id,  `Welcome Peter Fry \u{1F602}`),
-        bot.sendMessage(msg.chat.id,  `Welcome Stu \u{1F602}`),
-        bot.sendMessage(msg.chat.id,  `Welcome Will \u{1F602}`),
-        bot.sendMessage(msg.chat.id,  `Welcome grads \u{1F602}`),
-        bot.sendMessage(msg.chat.id,  `Special welcome to Chris Brown \u{1F602}`)
+        bot.sendMessage(msg.chat.id, `Welcome ${ msg.from.first_name } \u{1F602}`),
+        bot.sendMessage(msg.chat.id, `Welcome Peter Fry \u{1F602}`),
+        bot.sendMessage(msg.chat.id, `Welcome Stu \u{1F602}`),
+        bot.sendMessage(msg.chat.id, `Welcome Will \u{1F602}`),
+        bot.sendMessage(msg.chat.id, `Welcome grads \u{1F602}`),
+        bot.sendMessage(msg.chat.id, `Special welcome to Chris Brown \u{1F602}`)
     ]
 });
 
@@ -40,18 +54,18 @@ bot.on('/welcomeloop', (msg) => {
         `Welcome Stu `,
         `Welcome Will `,
         `Welcome grads `,
-        `Special welcome to Chris Brown ` 
+        `Special welcome to Chris Brown `
     ]
     let messagesToSend = []
-    
+
     for (welcome in messages) {
-        const ranNum = Math.floor(Math.random() * 20) + 1  
-        let emojiBoi = ((ranNum < 6) ? ""  : "\u{1F602}")
-        emojiBoi += (ranNum < 12) ? '': "\u{1F602}"
-        emojiBoi += (ranNum < 18) ? '': "\u{1F602}"
+        const ranNum = Math.floor(Math.random() * 20) + 1
+        let emojiBoi = ((ranNum < 6) ? "" : "\u{1F602}")
+        emojiBoi += (ranNum < 12) ? '' : "\u{1F602}"
+        emojiBoi += (ranNum < 18) ? '' : "\u{1F602}"
         bot.sendMessage(msg.chat.id, messages[welcome] + emojiBoi);
     }
-    
+
     // return messagesToSend;
 });
 
@@ -107,16 +121,16 @@ bot.on('newChatMembers', (msg) => {
     // console.log(msg.new_chat_members[0])
     // console.log(msg.new_chat_members[0].first_name)
     // // console.log(new_chat_members)
-    
-    
+
+
     let messages = [
         `Welcome Peter Fry `,
         `Welcome Stu `,
         `Welcome Will `,
         `Welcome grads `,
-        `Special welcome to Chris Brown ` 
+        `Special welcome to Chris Brown `
     ]
-    try {    
+    try {
         for (newMember in msg.new_chat_members) {
             // console.log('hello world chat members in loop')
             // console.log(msg.new_chat_members)
@@ -126,20 +140,19 @@ bot.on('newChatMembers', (msg) => {
             // console.log(msg.new_chat_members[newMember].first_name)
             messages.push(`Welcome ${ msg.new_chat_members[newMember].first_name }`)
         }
-        
+
         // console.log(`whole messages block is: `)
         // console.log(messages)
-        
-        
+
+
         for (welcome in messages) {
-            const ranNum = Math.floor(Math.random() * 20) + 1  
-            let emojiBoi = ((ranNum < 6) ? ""  : "\u{1F602}")
-            emojiBoi += (ranNum < 12) ? '': "\u{1F602}"
-            emojiBoi += (ranNum < 18) ? '': "\u{1F602}"
+            const ranNum = Math.floor(Math.random() * 20) + 1
+            let emojiBoi = ((ranNum < 6) ? "" : "\u{1F602}")
+            emojiBoi += (ranNum < 12) ? '' : "\u{1F602}"
+            emojiBoi += (ranNum < 18) ? '' : "\u{1F602}"
             bot.sendMessage(msg.chat.id, messages[welcome] + emojiBoi);
         }
-    }
-    catch (error) {
+    } catch (error) {
         console.log(`error is ` + error)
         return bot.sendMessage(msg.chat.id, `You broke me because of Aidan's poor error handling, I don't feel so good Mr Stark. \u{1F602} !`)
         return bot.sendMessage(msg.chat.id, error)
@@ -151,5 +164,5 @@ bot.on('error', (msg) => {
     console.log(`error generated`)
     return bot.sendMessage(msg.chat.id, `You broke me because of Aidan's poor error handling, I don't feel so good Mr Stark. \u{1F602} !`)
 })
- 
+
 bot.start();
