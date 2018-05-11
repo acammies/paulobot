deb1d2580dcca1fc9358fd0951045bc42b2d7715
acammies <acammies@redhat.com>
2018-05-11 15:53:09 +0100

    added promises so that messages are sent in the correct order

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const TeleBot = require('telebot');
+const Promise = require('bluebird');
 const TOKEN = process.env.TOKEN;
 const bot = new TeleBot(TOKEN);
 
@@ -33,10 +34,12 @@ bot.on('/welcomeall', (msg) => {
     ]
 });
 
+
+
 bot.on('/welcomeloop', (msg) => {
     console.log(`/welcomeloop triggered`)
     let messages = [
-        `Welcome ${ msg.chat.first_name } `,
+        `Welcome ${ msg.from.first_name } `,
         `Welcome Peter Fry `,
         `Welcome Stu `,
         `Welcome Will `,
@@ -52,6 +55,33 @@ bot.on('/welcomeloop', (msg) => {
         emojiBoi += (ranNum < 18) ? '' : "\u{1F602}"
         bot.sendMessage(msg.chat.id, messages[welcome] + emojiBoi);
     }
+
+    // return messagesToSend;
+});
+
+bot.on('/welcomepromise', (msg) => {
+    console.log(`/welcomepromise triggered`)
+    let messages = [
+        `Welcome Peter Fry `,
+        `Welcome Stu `,
+        `Welcome Will `,
+        `Welcome grads `,
+        `Special welcome to Chris Brown `
+    ]
+    messages.unshift(`Welcome ${ msg.from.first_name } `);
+    let messagesToSend = []
+
+    Promise.each(messages, welcome => {
+        const ranNum = Math.floor(Math.random() * 20) + 1
+        let emojiBoi = ((ranNum < 6) ? "" : "\u{1F602}")
+        emojiBoi += (ranNum < 12) ? '' : "\u{1F602}"
+        emojiBoi += (ranNum < 18) ? '' : "\u{1F602}"
+        return bot.sendMessage(msg.chat.id, welcome + emojiBoi);
+    })
+
+    // for (welcome in messages) {
+    //     bot.sendMessage(msg.chat.id, messages[welcome] + emojiBoi);
+    // }
 
     // return messagesToSend;
 });
@@ -125,20 +155,22 @@ bot.on('newChatMembers', (msg) => {
             // console.log(msg.new_chat_members[newMember])
             // console.log('first name is ')
             // console.log(msg.new_chat_members[newMember].first_name)
-            messages.push(`Welcome ${ msg.new_chat_members[newMember].first_name }`)
+            messages.unshift(`Welcome ${ msg.new_chat_members[newMember].first_name }`)
+            // console.log("messages is ")
+            // console.log(messages)
         }
 
-        // console.log(`whole messages block is: `)
-        // console.log(messages)
+        console.log(`whole messages block is: `)
+        console.log(messages)
 
-
-        for (welcome in messages) {
+        Promise.each(messages, welcome => {
             const ranNum = Math.floor(Math.random() * 20) + 1
             let emojiBoi = ((ranNum < 6) ? "" : "\u{1F602}")
             emojiBoi += (ranNum < 12) ? '' : "\u{1F602}"
             emojiBoi += (ranNum < 18) ? '' : "\u{1F602}"
-            bot.sendMessage(msg.chat.id, messages[welcome] + emojiBoi);
-        }
+            return bot.sendMessage(msg.chat.id, welcome + emojiBoi);
+        })
+
     } catch (error) {
         console.log(`error is ` + error)
         return bot.sendMessage(msg.chat.id, `You broke me because of Aidan's poor error handling, I don't feel so good Mr Stark. \u{1F602} !`)
